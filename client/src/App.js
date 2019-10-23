634f430030d7d7b3cf4e41d335a7c155bb6290c5
Madses <k.l.m.aartsen@hotmail.com>
2019-10-24 01:18:59 +0200

    setup client done

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React , {Fragment} from 'react';
+import Landing from './components/Landing';
+import './App.css';
+
+const App = () => (
+    <Fragment>
+      <Landing/>
+    </Fragment>
+)
+
+export default App;
