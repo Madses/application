0e5df8d1ce5d3026d27171b893fb77780c268acd
Kevin <k.l.m.aartsen@hotmail.com>
2019-10-29 17:19:56 +0100

    Implemented remove alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,18 @@
 import uuid from 'uuid';
 import {SET_ALERT,REMOVE_ALERT} from '../actions/types';
 
-export const setAlert =  (msg,alertType) => dispatch => {
+export const setAlert =  (msg,type) => dispatch => {
     const id =  uuid.v4();
     dispatch({
         type : SET_ALERT,
-        payload : {msg,alertType,id},
+        payload : {msg,type,id},
     });
+
+    setTimeout(() => {
+        dispatch({
+            type: REMOVE_ALERT,
+            payload: id
+        })
+    },5000)
 };
 
