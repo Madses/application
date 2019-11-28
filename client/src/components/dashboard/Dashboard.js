e813509f196601cdc1ea678ea52ca80712637894
Kevin <k.l.m.aartsen@hotmail.com>
2019-11-28 11:23:56 +0100

    made the dashboard work

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React , {Fragment ,useEffect} from 'react';
+import {connect} from 'react-redux';
+import {loadUser} from '../../actions/auth';
+import store from '../../store';
 
-export default function Dashboard() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+const Dashboard = ({user}) => {
+    return (<div>
+        <h1>{user ? (<div>{user.name}</div>) : (<div>loading...</div>)}</h1>
+    </div>)
+
+};
+
+const mapStateToProps = (state) => {
+    return {user : state.auth.user};
+};
+
+
+export default connect(mapStateToProps,{loadUser})(Dashboard);
